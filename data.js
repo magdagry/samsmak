e3cca41af15e93b9104110efc6d7fede2f8310b1
Lenka <magdagry16@gmail.com>
2023-08-09 18:23:28 +0200

    Changes

---
+++ data.js
@@ -0,0 +1,112 @@
+export const menu = [
+    {
+        id: 1,
+        data: "07.08.2023",
+        dayName: "Poniedziałek",
+        dishes: [
+            {
+                name: "Danie 11",
+                description: "Opis dania 11",
+                price: 11
+            },
+            {
+                name: "Danie 12",
+                description: "Opis dania 12",
+                price: 12
+            },
+            {
+                name: "Danie 13",
+                description: "Opis dania 13",
+                price: 13
+            },
+        ]
+    },
+    {
+        id: 2,
+        data: "08.08.2023",
+        dayName: "Wtorek",
+        dishes: [
+            {
+                name: "Danie 21",
+                description: "Opis dania 21",
+                price: 21
+            },
+            {
+                name: "Danie 12",
+                description: "Opis dania 22",
+                price: 22
+            },
+            {
+                name: "Danie 13",
+                description: "Opis dania 23",
+                price: 23
+            },
+        ]
+    },
+    {
+        id: 3,
+        data: "09.08.2023",
+        dayName: "Sroda",
+        dishes: [
+            {
+                name: "Danie 31",
+                description: "Opis dania 31",
+                price: 31
+            },
+            {
+                name: "Danie 32",
+                description: "Opis dania 32",
+                price: 12
+            },
+            {
+                name: "Danie 33",
+                description: "Opis dania 33",
+                price: 33
+            },
+        ]
+    },
+    {
+        id: 4,
+        data: "10.08.2023",
+        dayName: "Czwartek",
+        dishes: [
+            {
+                name: "Danie 41",
+                description: "Opis dania 41",
+                price: 41
+            },
+            {
+                name: "Danie 42",
+                description: "Opis dania 42",
+                price: 42
+            },
+            {
+                name: "Danie 43",
+                description: "Opis dania 43",
+                price: 43
+            },
+        ]
+    },
+    {
+        id: 5,
+        data: "11.08.2023",
+        dayName: "Piątek",
+        dishes: [
+            {
+                name: "Danie 51",
+                description: "Opis dania 51",
+                price: 51
+            },
+            {
+                name: "Danie 52",
+                description: "Opis dania 52",
+                price: 52
+            },
+            {
+                name: "Danie 53",
+                description: "Opis dania 53",
+                price: 53
+            },
+        ]
+    },
+]
